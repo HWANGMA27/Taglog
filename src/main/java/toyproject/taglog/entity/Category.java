e6c2f3b5027bc389973b3c389d32a78c8a540b12
hwangma27 <pikacu22@kakao.com>
2022-01-10 13:49:35 +0900

    [ADD] category 엔티티 생성 #2

---
+++ src/main/java/toyproject/taglog/entity/Category.java
@@ -0,0 +1,26 @@
+package toyproject.taglog.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String name;
+
+    @Column(name = "category_order")
+    private int order;
+}
