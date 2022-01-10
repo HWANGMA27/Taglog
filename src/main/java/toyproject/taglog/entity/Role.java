c59d22e1d0cfe210d17797d506acc4a996c589ef
hwangma27 <pikacu22@kakao.com>
2022-01-10 13:51:52 +0900

    [ADD] Role enum클래스 생성 #6

---
+++ src/main/java/toyproject/taglog/entity/Role.java
@@ -0,0 +1,16 @@
+package toyproject.taglog.entity;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN("ROLE_ADMIN", "ADMIN"),
+    USER("ROLE_USER", "USER"),
+    GUEST("ROLE_GUEST", "GUEST");
+
+    private final String key;
+    private final String title;
+}
