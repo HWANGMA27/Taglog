822235b1c1d6c62de04bc808972764b1c2052ee5
hwangma27 <pikacu22@kakao.com>
2022-01-11 16:02:01 +0900

    [MOD] 운영 배포 테스트를 위해 주석처리 #19

--- src/test/java/toyproject/taglog/TaglogApplicationTests.java
+++ src/test/java/toyproject/taglog/TaglogApplicationTests.java
@@ -2,8 +2,9 @@ package toyproject.taglog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@SpringBootTest
 class TaglogApplicationTests {
 
 	@Test
