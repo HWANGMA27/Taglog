bd76d039fb886ae4eb326bb512e801c08f4100c5
hwangma27 <pikacu22@kakao.com>
2022-01-11 13:26:25 +0900

    [ADD] Category Repository 인터페이스 생성 close #7

---
+++ src/main/java/toyproject/taglog/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package toyproject.taglog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import toyproject.taglog.entity.Category;
+import toyproject.taglog.entity.User;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("SELECT c FROM Category c WHERE c.user = :user ORDER BY order")
+    List<Category> findByUser(@Param("user") User user);
+
+    Category getByUserIdAndId(@Param("userId") Long userId, @Param("id") Long id);
+}
