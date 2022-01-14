13e043753c6be8d12f71247b142b441699cf4ffe
hwangma27 <pikacu22@kakao.com>
2022-01-14 21:11:14 +0900

    [ADD] NoteRepository 인터페이스 추가 close #23

---
+++ src/main/java/toyproject/taglog/repository/NoteRepository.java
@@ -0,0 +1,8 @@
+package toyproject.taglog.repository;
+
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import toyproject.taglog.entity.Note;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
