cb2120510cfa235054e6d36294e2a750a14e9d80
hwangma27 <pikacu22@kakao.com>
2022-01-14 21:13:27 +0900

    [ADD] TagDTO 추가 close #26

---
+++ src/main/java/toyproject/taglog/dto/TagDTO.java
@@ -0,0 +1,17 @@
+package toyproject.taglog.dto;
+
+import lombok.Data;
+
+@Data
+public class TagDTO {
+    private Long tagId;
+    private String name;
+
+    public TagDTO() {
+    }
+
+    public TagDTO(Long tagId, String name) {
+        this.tagId = tagId;
+        this.name = name;
+    }
+}
