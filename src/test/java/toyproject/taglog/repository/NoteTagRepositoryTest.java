6ca6de80c7d9b68f5f2c7d37b69af36a3e362691
hwangma27 <pikacu22@kakao.com>
2022-01-19 20:38:36 +0900

    [MOD] NoteTagRepositoryTest 검색 테스트 추가 #40

--- src/test/java/toyproject/taglog/repository/NoteTagRepositoryTest.java
+++ src/test/java/toyproject/taglog/repository/NoteTagRepositoryTest.java
@@ -12,6 +12,7 @@ import toyproject.taglog.entity.*;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 @Transactional
@@ -33,6 +34,7 @@ class NoteTagRepositoryTest {
     User user;
     Category category;
     Note note;
+    Tag tag, tag2;
 
     @BeforeEach
     public void beforeEach() throws Exception{
@@ -42,8 +44,8 @@ class NoteTagRepositoryTest {
         category = new Category("테스트 카테고리", 2, user);
         categoryRepository.save(category);
 
-        Tag tag = new Tag("테스트 태그");
-        Tag tag2 = new Tag("테스트 태그2");
+        tag = new Tag("테스트 태그");
+        tag2 = new Tag("테스트 태그2");
         tagRepository.save(tag);
         tagRepository.save(tag2);
 
@@ -60,6 +62,34 @@ class NoteTagRepositoryTest {
 
         em.flush();
         em.clear();
+
+    }
+
+    @DisplayName("유저 아이디로 noteTag & tag select 테스트")
+    @Test
+    public void findNoteTagByUserIdTest() throws Exception{
+        //
+        note = new Note("타이틀2", "컨텐츠2");
+        note.updateCategory(category);
+        note.updateNoteStatus("N");
+        note.updateUser(user);
+        noteRepository.save(note);
+
+        NoteTag noteTag = new NoteTag(note, tag, user);
+        NoteTag noteTag2 = new NoteTag(note, tag2, user);
+        noteTagRepository.save(noteTag);
+        noteTagRepository.save(noteTag2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<NoteTag> noteTagByUserId = noteTagRepository.findNoteTagByUserId(user.getId());
+        List<Tag> tags = noteTagByUserId.stream()
+                .map(NoteTag::getTag).distinct().collect(Collectors.toList());
+        //then
+        Assertions.assertThat(noteTagByUserId.size()).isEqualTo(4);
+        Assertions.assertThat(tags.size()).isEqualTo(2);
 
     }
 
