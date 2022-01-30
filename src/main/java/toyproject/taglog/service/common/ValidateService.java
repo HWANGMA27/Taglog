4465fb99e909de6ccd37ebc826170ac829244f43
hwangma27 <pikacu22@kakao.com>
2022-01-30 21:34:15 +0900

    [ADD] 변수 validation 확인하는 공통 서비스 추가 #51

---
+++ src/main/java/toyproject/taglog/service/common/ValidateService.java
@@ -0,0 +1,37 @@
+package toyproject.taglog.service.common;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toyproject.taglog.entity.Category;
+import toyproject.taglog.entity.Note;
+import toyproject.taglog.entity.User;
+import toyproject.taglog.exception.invalid.InvalidateCategoryException;
+import toyproject.taglog.exception.invalid.InvalidateNoteException;
+import toyproject.taglog.exception.invalid.InvalidateUserException;
+import toyproject.taglog.repository.CategoryRepository;
+import toyproject.taglog.repository.NoteRepository;
+import toyproject.taglog.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+@Aspect
+@Transactional(readOnly = true)
+public class ValidateService {
+    private final NoteRepository noteRepository;
+    public final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+
+    public Note validateNote(Long noteId, String delYn){
+        return noteRepository.findByIdAndDelYn(noteId, delYn).orElseThrow(InvalidateNoteException::new);
+    }
+
+    public Category validateCategory(Long categoryId){
+        return categoryRepository.findById(categoryId).orElseThrow(InvalidateCategoryException::new);
+    }
+
+    public User validateUser(Long userId){
+        return userRepository.findById(userId).orElseThrow(InvalidateUserException::new);
+    }
+}
